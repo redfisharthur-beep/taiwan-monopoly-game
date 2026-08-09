6cbcad059f3a0e62d449c2b675e44f70a37eae37
redfisharthur-beep <redfisharthur@gmail.com>
2026-08-09 14:50:26 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,430 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'taiwan', 'public')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// 24格遊戲地圖格子定義與價格
+const board = [
+    { name: "起點", type: "start", price: 0 },
+    { name: "宜蘭", type: "land", group: "East", price: 2000, owner: null },
+    { name: "花蓮", type: "land", group: "East", price: 2000, owner: null },
+    { name: "機會", type: "card", price: 0 },
+    { name: "台東", type: "land", group: "East", price: 2000, owner: null },
+    { name: "旅行中", type: "travel", price: 0 },
+    { name: "屏東", type: "land", group: "East", price: 2000, owner: null },
+    { name: "高雄", type: "land", group: "South", price: 2500, owner: null },
+    { name: "綠島", type: "land", group: "South", price: 2500, owner: null },
+    { name: "台南", type: "land", group: "South", price: 2500, owner: null },
+    { name: "澎湖", type: "land", group: "South", price: 2500, owner: null },
+    { name: "命運", type: "card", price: 0 },
+    { name: "嘉義", type: "land", group: "Central", price: 2300, owner: null },
+    { name: "雲林", type: "land", group: "Central", price: 2300, owner: null },
+    { name: "彰化", type: "land", group: "Central", price: 2300, owner: null },
+    { name: "南投", type: "action", price: 0 },
+    { name: "台中", type: "land", group: "North-C", price: 2800, owner: null },
+    { name: "苗栗", type: "land", group: "North-C", price: 2800, owner: null },
+    { name: "新竹", type: "land", group: "North-C", price: 2800, owner: null },
+    { name: "桃園", type: "action", price: 0 },
+    { name: "台北", type: "land", group: "North", price: 3000, owner: null },
+    { name: "新北", type: "land", group: "North", price: 3000, owner: null },
+    { name: "金門", type: "land", group: "North", price: 3000, owner: null },
+    { name: "基隆", type: "land", group: "North", price: 3000, owner: null }
+];
+
+// 4 個玩家的專屬設定
+const playerProfiles = [
+    { img: "pig.png", color: "#FF9F9F" },
+    { img: "cat.png", color: "#9FBFFF" },
+    { img: "dog.png", color: "#9FDF9F" },
+    { img: "bear.png", color: "#FFDF9F" }
+];
+let rooms = {}; 
+
+io.on('connection', (socket) => {
+    socket.emit('roomList', rooms);
+
+    socket.on('createRoom', ({ roomName, playerName }) => {
+        if (rooms[roomName]) {
+            socket.emit('errorMsg', '房間名稱已被使用！');
+            return;
+        }
+        rooms[roomName] = {
+            roomName: roomName,
+            host: socket.id,
+            round: 1,
+            players: [{
+                id: socket.id,
+                name: playerName,
+                money: 20000,
+                pos: 0,
+                profile: playerProfiles[0],
+                skipTurns: 0
+            }],
+            started: false,
+            turnIndex: 0,
+            board: JSON.parse(JSON.stringify(board))
+        };
+        socket.join(roomName);
+        socket.roomName = roomName;
+        io.emit('roomList', rooms);
+        io.to(roomName).emit('updateGame', rooms[roomName]);
+    });
+
+    socket.on('joinRoom', ({ roomName, playerName }) => {
+        let room = rooms[roomName];
+        if (!room) return socket.emit('errorMsg', '房間不存在！');
+        if (room.started) return socket.emit('errorMsg', '遊戲已經開始囉！');
+        if (room.players.length >= 4) return socket.emit('errorMsg', '房間已滿（最多4人）！');
+
+        let profileIndex = room.players.length;
+        room.players.push({
+            id: socket.id,
+            name: playerName,
+            money: 20000,
+            pos: 0,
+            profile: playerProfiles[profileIndex],
+            skipTurns: 0
+        });
+
+        socket.join(roomName);
+        socket.roomName = roomName;
+        io.emit('roomList', rooms);
+        io.to(roomName).emit('updateGame', room);
+    });
+
+    socket.on('startGame', () => {
+        let room = rooms[socket.roomName];
+        if (room && room.host === socket.id) {
+            room.started = true;
+            io.emit('roomList', rooms);
+            io.to(socket.roomName).emit('updateGame', room);
+            io.to(socket.roomName).emit('log', '遊戲正式開始！由 ' + room.players[0].name + ' 先攻。');
+            startTurn(room);
+        }
+    });
+
+    socket.on('buyDecision', ({ buy }) => {
+        let room = rooms[socket.roomName];
+        if (!room) return;
+        let currentPlayer = room.players[room.turnIndex];
+        let currentTile = room.board[currentPlayer.pos];
+
+        if (buy && currentTile.type === 'land' && !currentTile.owner) {
+            if (currentPlayer.money >= currentTile.price) {
+                currentPlayer.money -= currentTile.price;
+                currentTile.owner = currentPlayer.id;
+                let msg = `🏠 ${currentPlayer.name} 花費 ${currentTile.price} <br>成功買下【${currentTile.name}】！`;
+                io.to(socket.roomName).emit('centerAlert', msg);
+                io.to(socket.roomName).emit('log', msg.replace(/<br>/g, ' '));
+            }
+        }
+        if (!nextTurn(room)) {
+            io.to(room.roomName).emit('updateGame', room);
+            startTurn(room);
+        }
+    });
+
+    socket.on('buyoutLand', () => {
+        let room = rooms[socket.roomName];
+        if (!room) return;
+        let currentPlayer = room.players[room.turnIndex];
+        let currentTile = room.board[currentPlayer.pos];
+
+        if (currentTile.type === 'land' && currentTile.owner && currentTile.owner !== currentPlayer.id) {
+            let buyoutPrice = currentTile.price * 3;
+            if (currentPlayer.money >= buyoutPrice) {
+                currentPlayer.money -= buyoutPrice;
+                let ownerObj = room.players.find(p => p.id === currentTile.owner);
+                if (ownerObj) ownerObj.money += buyoutPrice;
+                
+                currentTile.owner = currentPlayer.id;
+                let msg = `💰 ${currentPlayer.name} 花費 ${buyoutPrice} (3倍) <br>強制收購了【${currentTile.name}】！`;
+                io.to(socket.roomName).emit('centerAlert', msg);
+                io.to(socket.roomName).emit('log', msg.replace(/<br>/g, ' '));
+                if (!nextTurn(room)) {
+                    io.to(room.roomName).emit('updateGame', room);
+                    startTurn(room);
+                }
+            }
+        }
+    });
+
+    socket.on('skipBuyout', () => {
+        let room = rooms[socket.roomName];
+        if (room) {
+            if (!nextTurn(room)) {
+                io.to(room.roomName).emit('updateGame', room);
+                startTurn(room);
+            }
+        }
+    });
+
+    socket.on('disconnect', () => {
+        for (let rName in rooms) {
+            let room = rooms[rName];
+            room.players = room.players.filter(p => p.id !== socket.id);
+            if (room.players.length === 0) {
+                delete rooms[rName];
+            } else if (room.host === socket.id) {
+                room.host = room.players[0].id;
+            }
+            io.emit('roomList', rooms);
+            io.to(rName).emit('updateGame', room);
+        }
+    });
+});
+
+function startTurn(room) {
+    if (!room.started) return;
+    let currentPlayer = room.players[room.turnIndex];
+
+    if (currentPlayer.skipTurns > 0) {
+        currentPlayer.skipTurns--;
+        let msg = `${currentPlayer.name} 暫停中，自動跳過，剩餘 ${currentPlayer.skipTurns} 回合。`;
+        io.to(room.roomName).emit('centerAlert', msg);
+        io.to(room.roomName).emit('log', msg);
+        io.to(room.roomName).emit('updateGame', room);
+        
+        setTimeout(() => {
+            if (!nextTurn(room)) {
+                startTurn(room);
+            }
+        }, 2000);
+        return;
+    }
+
+    io.to(room.roomName).emit('updateGame', room);
+
+    setTimeout(() => {
+        if (!room.started) return;
+        let dice = Math.floor(Math.random() * 6) + 1;
+        let oldPos = currentPlayer.pos;
+        currentPlayer.pos = (currentPlayer.pos + dice) % room.board.length;
+        let currentTile = room.board[currentPlayer.pos];
+
+        let msg = `🎲 ${currentPlayer.name} 擲出了 ${dice} 點！<br>來到了【${currentTile.name}】`;
+        
+        if (currentPlayer.pos < oldPos || (currentPlayer.pos === 0 && dice > 0)) {
+            currentPlayer.money += 10000;
+            msg += `<br>經過起點，領取薪水 10000！`;
+        }
+        
+        io.to(room.roomName).emit('centerAlert', msg);
+        io.to(room.roomName).emit('log', msg.replace(/<br>/g, ' '));
+        io.to(room.roomName).emit('updateGame', room);
+
+        setTimeout(() => {
+            handleTileEvent(room, currentPlayer, currentTile);
+        }, 2000);
+    }, 1500);
+}
+
+function handleTileEvent(room, player, tile) {
+    if (tile.type === 'land') {
+        if (!tile.owner) {
+            io.to(player.id).emit('promptBuy', { price: tile.price, name: tile.name });
+        } else if (tile.owner === player.id) {
+            io.to(room.roomName).emit('log', `${player.name} 回到了自己的地盤 ${tile.name}。`);
+            if (!nextTurn(room)) {
+                io.to(room.roomName).emit('updateGame', room);
+                startTurn(room);
+            }
+        } else {
+            let ownerObj = room.players.find(p => p.id === tile.owner);
+            let sameGroupCount = room.board.filter(b => b.group === tile.group && b.owner === tile.owner).length;
+            let toll = Math.round(tile.price * 0.3 * Math.pow(2, sameGroupCount - 1));
+
+            if (player.money >= toll) {
+                player.money -= toll;
+                ownerObj.money += toll;
+                let msg = `💸 ${player.name} 踩到別人地盤<br>支付過路費 ${toll} 給 ${ownerObj.name}！`;
+                io.to(room.roomName).emit('centerAlert', msg);
+                io.to(room.roomName).emit('log', msg.replace(/<br>/g, ' '));
+                io.to(room.roomName).emit('updateGame', room);
+                
+                setTimeout(() => {
+                    io.to(player.id).emit('promptBuyout', { price: tile.price * 3, money: player.money, name: tile.name });
+                }, 2000);
+            } else {
+                player.money = 0;
+                player.pos = 0;
+                player.skipTurns = 2;
+                let msg = `💥 ${player.name} 破產！<br>無力支付過路費，強制回到起點，暫停 2 回合（無薪水）！`;
+                io.to(room.roomName).emit('centerAlert', msg);
+                io.to(room.roomName).emit('log', msg.replace(/<br>/g, ' '));
+                if (!nextTurn(room)) {
+                    io.to(room.roomName).emit('updateGame', room);
+                    startTurn(room);
+                }
+            }
+        }
+    } else if (tile.type === 'card') {
+        let isChance = (tile.name === "機會");
+        handleCardEvent(room, player, isChance);
+    } else if (tile.name === "南投") {
+        let msg = `🎲 ${player.name} 來到南投！<br>自動再擲一次骰子！`;
+        io.to(room.roomName).emit('centerAlert', msg);
+        io.to(room.roomName).emit('log', msg.replace(/<br>/g, ' '));
+
+        setTimeout(() => {
+            let dice = Math.floor(Math.random() * 6) + 1;
+            let oldPos = player.pos;
+            player.pos = (player.pos + dice) % room.board.length;
+            let currentTile = room.board[player.pos];
+
+            let subMsg = `🎲 ${player.name} 再次擲出了 ${dice} 點！<br>來到了【${currentTile.name}】`;
+            
+            if (player.pos < oldPos || (player.pos === 0 && dice > 0)) {
+                player.money += 10000;
+                subMsg += `<br>經過起點，領取薪水 10000！`;
+            }
+            
+            io.to(room.roomName).emit('centerAlert', subMsg);
+            io.to(room.roomName).emit('log', subMsg.replace(/<br>/g, ' '));
+            io.to(room.roomName).emit('updateGame', room);
+
+            setTimeout(() => {
+                handleTileEvent(room, player, currentTile);
+            }, 2000);
+        }, 2000);
+
+    } else if (tile.name === "桃園") {
+        player.pos = 5; 
+        player.skipTurns = 3;
+        let msg = `✈️ ${player.name} 從桃園出國囉！<br>強制移動到「旅行中」並暫停 3 回合！`;
+        io.to(room.roomName).emit('centerAlert', msg);
+        io.to(room.roomName).emit('log', msg.replace(/<br>/g, ' '));
+        if (!nextTurn(room)) {
+            io.to(room.roomName).emit('updateGame', room);
+            startTurn(room);
+        }
+    } else {
+        if (!nextTurn(room)) {
+            io.to(room.roomName).emit('updateGame', room);
+            startTurn(room);
+        }
+    }
+}
+
+function handleCardEvent(room, player, isChance) {
+    let effects = [];
+    if (isChance) {
+        effects = [
+            { type: 'money', val: 10000, desc: '獲得獎勵金錢 10000！', isReward: true },
+            { type: 'money', val: 500, desc: '獲得獎勵金錢 500！', isReward: true },
+            { type: 'land_get', desc: '幸運降臨！隨機獲得一塊未被佔領的地盤！', isReward: true },
+            { type: 'money', val: -500, desc: '遭遇倒霉事...失去 500！', isReward: false },
+            { type: 'land_lose', desc: '真不幸...隨機失去一塊已佔領的地盤！', isReward: false },
+            { type: 'land_grab_three', desc: '逆轉勝來了！隨機豪取 3 塊地盤（無論是否有所屬）！', isReward: true },
+            { type: 'money', val: 30000, desc: '天選之人，恭喜中樂透！獲得 3 萬獎金！', isReward: true }
+        ];
+    } else {
+        effects = [
+            { type: 'money', val: 500, desc: '獲得獎勵金錢 500！', isReward: true },
+            { type: 'land_get', desc: '幸運降臨！隨機獲得一塊未被佔領的地盤！', isReward: true },
+            { type: 'money', val: -10000, desc: '遭遇大災難...失去 10000！', isReward: false },
+            { type: 'money', val: -500, desc: '遭遇倒霉事...失去 500！', isReward: false },
+            { type: 'land_lose', desc: '真不幸...隨機失去一塊已佔領的地盤！', isReward: false },
+            { type: 'travel_forced', desc: '出差辛苦了~強制移動到旅行中，暫停 2 回合！', isReward: false }
+        ];
+    }
+
+    let choice = effects[Math.floor(Math.random() * effects.length)];
+    let cardTypeName = isChance ? "機會" : "命運";
+    let msg = `🃏 抽到【${cardTypeName}】卡牌！<br>`;
+
+    if (choice.type === 'money') {
+        player.money += choice.val;
+        if (player.money < 0) player.money = 0;
+        let colorStyle = choice.isReward ? 'color:#FF7F7F' : 'color:#7F7FFF';
+        msg += `<span style="${colorStyle}">${choice.desc}</span>`;
+    } else if (choice.type === 'land_get') {
+        let unownedLands = room.board.filter(b => b.type === 'land' && !b.owner);
+        if (unownedLands.length > 0) {
+            let targetLand = unownedLands[Math.floor(Math.random() * unownedLands.length)];
+            targetLand.owner = player.id;
+            msg += `<span style="color:#FF7F7F">${choice.desc} 獲得了【${targetLand.name}】！</span>`;
+        } else {
+            player.money += 2000;
+            msg += `<span style="color:#FF7F7F">場上已無未被佔領的地盤，改為獲得獎勵 2000！</span>`;
+        }
+    } else if (choice.type === 'land_lose') {
+        let playerLands = room.board.filter(b => b.type === 'land' && b.owner === player.id);
+        if (playerLands.length > 0) {
+            let targetLand = playerLands[Math.floor(Math.random() * playerLands.length)];
+            let landName = targetLand.name;
+            targetLand.owner = null;
+            msg += `<span style="color:#7F7FFF">${choice.desc} 失去了【${landName}】（變回無人佔領）！</span>`;
+        } else {
+            msg += `<span style="color:#7F7FFF">你目前名下沒有地盤可失去，逃過一劫！</span>`;
+        }
+    } else if (choice.type === 'land_grab_three') {
+        let allLands = room.board.filter(b => b.type === 'land');
+        let grabbedNames = [];
+        for (let i = 0; i < 3; i++) {
+            if (allLands.length === 0) break;
+            let randomIndex = Math.floor(Math.random() * allLands.length);
+            let targetLand = allLands.splice(randomIndex, 1)[0];
+            targetLand.owner = player.id;
+            grabbedNames.push(targetLand.name);
+        }
+        if (grabbedNames.length > 0) {
+            msg += `<span style="color:#FF7F7F">${choice.desc} 成功奪下：【${grabbedNames.join('、')}】！</span>`;
+        } else {
+            msg += `<span style="color:#FF7F7F">場上沒有可取的地盤！</span>`;
+        }
+    } else if (choice.type === 'travel_forced') {
+        player.pos = 5;
+        player.skipTurns = 2;
+        msg += `<span style="color:#7F7FFF">${choice.desc}</span>`;
+    }
+
+    io.to(room.roomName).emit('centerAlert', msg);
+    io.to(room.roomName).emit('log', msg.replace(/<br>/g, ' ').replace(/<[^>]+>/g, ''));
+    if (!nextTurn(room)) {
+        io.to(room.roomName).emit('updateGame', room);
+        startTurn(room);
+    }
+}
+
+function nextTurn(room) {
+    room.turnIndex = (room.turnIndex + 1) % room.players.length;
+    if (room.turnIndex === 0) {
+        room.round++;
+        if (room.round > 20) {
+            room.started = false;
+            let ranking = room.players.map(p => {
+                let landValue = room.board.filter(b => b.owner === p.id).reduce((sum, b) => sum + b.price, 0);
+                let totalAssets = p.money + landValue;
+                return {
+                    name: p.name,
+                    money: p.money,
+                    landValue: landValue,
+                    totalAssets: totalAssets,
+                    profile: p.profile
+                };
+            });
+            ranking.sort((a, b) => b.totalAssets - a.totalAssets);
+            
+            io.to(room.roomName).emit('gameOver', ranking);
+
+            delete rooms[room.roomName];
+            io.emit('roomList', rooms);
+            return true;
+        }
+    }
+    return false;
+}
+
+server.listen(3000, () => {
+    console.log('大富翁伺服器已啟動 localhost:3000');
+});
